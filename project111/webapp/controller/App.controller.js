440e1e344bc7781cb5b95498faef4ecd880abc7e
SOWMYA MANUKONDA <sowmya.surekha@yash.com>
2023-05-19 00:29:34 +0000

    This is my project111

---
+++ project111/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("project111.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ project111/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project111/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
